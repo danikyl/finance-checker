357d7a191a86ca2f3271520c795adfeca3bd1a52
Daniel De Marco <daniel.m.fucci@gmail.com>
2023-04-03 13:58:05 -0300

    Adding unit test

--- service/finance_service_impl_test.go
+++ service/finance_service_impl_test.go
@@ -1,1 +1,28 @@
 package service
+
+import (
+	"testing"
+
+	"github.com/danikyl/finance-helper/model"
+)
+
+func TestGenerateReport(t *testing.T) {
+	financeService := NewFinanceServiceImpl()
+	request := model.FinanceReportRequest{
+		MonthlyInterestsRate: 0.01,
+		RentPrice:            5000,
+		InstallmentValue:     8000,
+		PeriodInMonths:       120,
+	}
+	report := financeService.GenerateReport(request)
+	got := report.HouseAmountAfterInterests
+	want := float64(960000)
+	if got != want {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+	got = report.SavedMoneyRentInsteadInstallment
+	want = 697017.2290558208
+	if got != want {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
